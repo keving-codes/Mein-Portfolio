d1490e64bd659b61fee94f3bb28e8d15823417a9
Kevin Gering <kg@kevingering.tech>
2025-06-21 22:29:48 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Space Grotesk'", "sans-serif"],
+      },
+      colors: {
+        primary: "#0ea5e9",
+        accent: "#7c3aed",
+        dark: "#111827",
+        light: "#f9fafb",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
